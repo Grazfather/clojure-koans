05d61dd09d039d79723784aeebb0fbeb5f56820f
Grazfather <grazfather@gmail.com>
2020-10-08 18:38:10 -0400

    Levels 1-15

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,27 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      false
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= (count coll) 0)
+    []
+    (conj (recursive-reverse (rest coll)) (first coll))))
 
 (defn factorial [n]
-  __)
+  (loop [n n acc 1]
+    (cond
+      (= n 0) acc
+      (= n 1) acc
+      :else (recur (- n 1) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
